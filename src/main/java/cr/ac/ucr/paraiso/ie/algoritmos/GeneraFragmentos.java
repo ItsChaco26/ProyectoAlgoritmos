4381a6b09d227698d0c11674c43eb1da014dfc49
Aarón Chacón <aaronchacon2601@gmail.com>
2023-06-27 18:48:01 -0600

    GeneraFragmentos

--- src/main/java/cr/ac/ucr/paraiso/ie/algoritmos/GeneraFragmentos.java
+++ src/main/java/cr/ac/ucr/paraiso/ie/algoritmos/GeneraFragmentos.java
@@ -16,6 +16,23 @@ public class GeneraFragmentos {
             escribirEnArchivo(fragmento);
         }
     }
+
+    public String leerContenidoArchivo(String nombreArchivo) {
+        StringBuilder contenido = new StringBuilder();
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(nombreArchivo))) {
+            String linea;
+
+            while ((linea = reader.readLine()) != null) {
+                contenido.append(linea).append("\n");
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Error al leer el archivo: " + e.getMessage());
+        }
+
+        return contenido.toString();
+    }
+
     public void escribirEnArchivo(String fragmento) {
         String nombreArchivo = "fragmentos.txt";
         try {
--- src/main/java/cr/ac/ucr/paraiso/ie/algoritmos/Main.java
+++ src/main/java/cr/ac/ucr/paraiso/ie/algoritmos/Main.java
@@ -5,7 +5,7 @@ package cr.ac.ucr.paraiso.ie.algoritmos;
 public class Main {
     public static void main(String[] args) {
         GeneraFragmentos gF = new GeneraFragmentos();
-        String nombreArchivoTexto = "";
+        String nombreArchivoTexto = gF.leerContenidoArchivo("archivo.txt");
         int cantidadFragmentos = 50;
         int longitudPromedio = 10;
         gF.generarFragmentos(nombreArchivoTexto, cantidadFragmentos, longitudPromedio);
