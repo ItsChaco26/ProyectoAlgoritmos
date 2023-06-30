eae49d8e6a4369d1e5a5dfa7a330ee2a01b18a84
Aarón Chacón <aaronchacon2601@gmail.com>
2023-06-29 21:20:40 -0600

    GeneraFragmentos

--- src/main/java/cr/ac/ucr/paraiso/ie/algoritmos/Fragmentos/EmsamblajeFragmentos.java
+++ src/main/java/cr/ac/ucr/paraiso/ie/algoritmos/Fragmentos/EmsamblajeFragmentos.java
@@ -1,4 +1,26 @@
 package cr.ac.ucr.paraiso.ie.algoritmos.Fragmentos;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class EmsamblajeFragmentos {
+
+    public void emsamblarFragmentos(String nombreArchivo) {
+        List<String> fragmentos = new ArrayList<String>();
+        try {
+            java.util.Scanner scanner = new java.util.Scanner(new java.io.File(nombreArchivo));
+            while (scanner.hasNextLine()) {
+                String fragmento = scanner.nextLine();
+                fragmentos.add(fragmento);
+            }
+            scanner.close();
+        } catch (IOException e) {
+            throw new RuntimeException("Error al leer el archivo: " + e.getMessage());
+        }
+        System.out.println(fragmentos.toString());
+    }
+
+
+
 }
--- src/main/java/cr/ac/ucr/paraiso/ie/algoritmos/Main.java
+++ src/main/java/cr/ac/ucr/paraiso/ie/algoritmos/Main.java
@@ -1,6 +1,7 @@
 package cr.ac.ucr.paraiso.ie.algoritmos;
 
 
+import cr.ac.ucr.paraiso.ie.algoritmos.Fragmentos.EmsamblajeFragmentos;
 import cr.ac.ucr.paraiso.ie.algoritmos.Fragmentos.GeneraFragmentos;
 import cr.ac.ucr.paraiso.ie.algoritmos.Utility.GestionaArchivo;
 
@@ -8,9 +9,12 @@ public class Main {
     public static void main(String[] args) {
         GeneraFragmentos gF = new GeneraFragmentos();
         GestionaArchivo gA = new GestionaArchivo();
+        EmsamblajeFragmentos eF = new EmsamblajeFragmentos();
         String nombreArchivoTexto = gA.leerContenidoArchivo("archivo.txt");
-        int cantidadFragmentos = 50;
+        int cantidadFragmentos = 200;
         int longitudPromedio = 10;
         gF.generarFragmentos(nombreArchivoTexto, cantidadFragmentos, longitudPromedio);
+        eF.emsamblarFragmentos("fragmentos.txt");
     }
+
 }
