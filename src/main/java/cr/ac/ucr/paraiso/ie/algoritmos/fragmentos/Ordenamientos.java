f9ceb9f3e97edf9341de73972ab21681566ba4f5
Aarón Chacón <aaronchacon2601@gmail.com>
2023-07-12 14:07:33 -0600

    Ordenamiento

--- src/main/java/cr/ac/ucr/paraiso/ie/algoritmos/fragmentos/Ordenamientos.java
+++ src/main/java/cr/ac/ucr/paraiso/ie/algoritmos/fragmentos/Ordenamientos.java
@@ -2,10 +2,7 @@ package cr.ac.ucr.paraiso.ie.algoritmos.fragmentos;
 
 import cr.ac.ucr.paraiso.ie.algoritmos.utility.GestionaArchivo;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
@@ -13,67 +10,51 @@ import java.util.List;
 public class Ordenamientos {
     GestionaArchivo gA = new GestionaArchivo();
 
-    List<String> fragmentos = gA.leerArchivoDANI("fragmentos.txt");
+    public String ordenarPorLongitud() {
+        List<String> fragmentos = gA.leerArchivoDANI("fragmentos.txt");
+        if(fragmentos.isEmpty()){
+            return "No hay fragmentos disponibles.";
+        }
+        else{
+            Collections.sort(fragmentos, Comparator.comparingInt(String::length));
+            StringBuilder resultado = new StringBuilder();
 
-  public String ordenarPorLongitud() {
+            for (String fragmento : fragmentos) {
+                resultado.append(fragmento + "\n");
+            }
 
-      if(fragmentos.isEmpty()){
-          return "No hay fragmentos disponibles.";
-      }
-
-      Collections.sort(fragmentos, Comparator.comparingInt(String::length));
-
-      StringBuilder resultado = new StringBuilder();
-
-
-      for (String fragmento : fragmentos) {
-          resultado.append(fragmento);
-      }
-
-      return resultado.toString();
-  }
+            return resultado.toString();}
+    }
 
 
     public String ordenarAlfabeticamente() {
-        StringBuilder resultado = new StringBuilder();
-        if(fragmentos.isEmpty()){
+        List<String> fragmentos = gA.leerArchivoDANI("fragmentos.txt");
+        if (fragmentos.isEmpty()) {
             return "No hay fragmentos disponibles.";
-        }else {
+        } else {
             Collections.sort(fragmentos);
+            StringBuilder resultado = new StringBuilder();
+
             for (String fragmento : fragmentos) {
-                resultado.append(fragmento);
+                resultado.append(fragmento + "\n");
             }
+
+            return resultado.toString();
         }
-        return resultado.toString();
     }
 
-    private static List<String> readLinesFromFile(String filePath) throws IOException {
-        List<String> lines = new ArrayList<>();
-        BufferedReader reader = new BufferedReader(new FileReader(filePath));
-        String line;
-
-        while ((line = reader.readLine()) != null) {
-            lines.add(line);
-        }
-
-        reader.close();
-        return lines;
-    }
-
-    public static void buscarPalabrasClave(String filePath, String keyword) throws IOException {
-        List<String> lineas = readLinesFromFile(filePath);
-        BufferedReader reader = new BufferedReader(new FileReader(filePath));
-        String linea;
-
-        while ((linea = reader.readLine()) != null) {
-            lineas.add(linea);
-        }
-        reader.close();
-
-        for (String lineaActual : lineas) {
-            if (lineaActual.contains(keyword)) {
-                System.out.println(lineaActual);
+    public String buscarPalabrasClave(String filePath, String keyword) throws IOException {
+        List<String> fragmentos = gA.leerArchivoDANI(filePath);
+        if (fragmentos.isEmpty()) {
+            return "No hay fragmentos disponibles.";
+        } else {
+            StringBuilder resultado = new StringBuilder();
+            for (String fragmento : fragmentos) {
+                if (fragmento.contains(keyword)) {
+                    resultado.append(fragmento + "\n");
+                }
             }
+            return resultado.toString();
         }
     }
 
--- src/main/java/cr/ac/ucr/paraiso/ie/algoritmos/utility/Menu.java
+++ src/main/java/cr/ac/ucr/paraiso/ie/algoritmos/utility/Menu.java
@@ -1,6 +1,8 @@
 package cr.ac.ucr.paraiso.ie.algoritmos.utility;
 
+import cr.ac.ucr.paraiso.ie.algoritmos.fragmentos.EnsamblajeFragmentos;
 import cr.ac.ucr.paraiso.ie.algoritmos.fragmentos.GeneraFragmentos;
+import cr.ac.ucr.paraiso.ie.algoritmos.fragmentos.Ordenamientos;
 
 import java.util.Scanner;
 
@@ -8,10 +10,12 @@ import java.util.Scanner;
 
 
 public class Menu {
-    public void menu(){
+    public void menuPrincipal(){
 
         GeneraFragmentos gF = new GeneraFragmentos();
         GestionaArchivo gA = new GestionaArchivo();
+        Ordenamientos ordenamientos = new Ordenamientos();
+        EnsamblajeFragmentos eF = new EnsamblajeFragmentos();
         Scanner sn = new Scanner(System.in);
         int opcion = 0;
 
@@ -23,24 +27,33 @@ public class Menu {
                     .append("3): ").append("Ordenar fragmentos, longitud\n")
                     .append("4): ").append("Ordenar fragmentos mayores o menores a un valor dado\n")
                     .append("5): ").append("Buscar palabras claves en los fragmentos\n")
-                    .append("6): ").append("Salir\n");
+                    .append("6): ").append("Reconstruir fragmentos\n")
+                    .append("7): ").append("Salir\n");
             System.out.println(sb);
             try {
                 opcion = sn.nextInt(); //Se asigna un dato tipo int a la variable opción para proceder a eveluarla en el switch y ejecutar un método o acciones especificas
                 switch (opcion) {
                     case 1:
                         System.out.println("Digite el número de fragmentos a generar:");
+
                         int cantidadFragmentos = sn.nextInt();
+
                         System.out.println("Digite la longitud promedio de los fragmentos:");
+
                         int longitudPromedio = sn.nextInt();
-                        //gF.generarFragmentos(gA.leerContenidoArchivo("archivo.txt"), cantidadFragmentos, longitudPromedio);
+
+                        String textoOriginal = gA.leerContenidoArchivo("archivo.txt");
+
+                        gF.generarFragmentos(textoOriginal, cantidadFragmentos, longitudPromedio);
+
                         System.out.println("Fragmentos generados y guardados en el archivo.");
+
                         break;
                     case 2:
-                        System.out.println("En proceso...");
+                        System.out.println(ordenamientos.ordenarAlfabeticamente());
                         break;
                     case 3:
-                        System.out.println("En proceso...");
+                        System.out.println(ordenamientos.ordenarPorLongitud());
                         break;
                     case 4:
                         System.out.println("En proceso...");
@@ -49,6 +62,9 @@ public class Menu {
                         System.out.println("En proceso...");
                         break;
                     case 6:
+                        System.out.println(eF.reconstruirTexto(gA.leerArchivoDANI("fragmentos.txt")));
+                        break;
+                    case 7:
                         System.exit(0); //Se termina la ejecución del programa.
                         break;
                     default:
@@ -60,6 +76,7 @@ public class Menu {
                 sn.nextLine();
             }
 
-    }
+        }
 
-}}
+    }
+}
